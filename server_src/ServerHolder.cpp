7da63b619a043a66b7c256772935cce9ff07d98b
Rama-PC <ramaa196@gmail.com>
2020-11-30 22:50:51 -0300

    borro cosas que no van

--- server_src/ServerHolder.cpp
+++ server_src/ServerHolder.cpp
@@ -11,28 +11,7 @@ ServerHolder::ServerHolder(int argc, char** argv):
 }
 
 void ServerHolder::run(){
-    float step_size = 0.1;
-    Mapa mapa(20,20);
-    float i;
-    bool lugar_vacio = false;
-    //Mirando hacia la izquierda
-    //Poner 00 despues de la coma
-    Coordinates angulo(-1,0);
-    Coordinates posicionIni(11, 15);
-    Jugador jugador(posicionIni, angulo, mapa);
-    DirAdelante dir;
-    for (i = 0.0; i < 1.0; i+=step_size)
-        jugador.mover(&dir);
-    Posicionable* resultado = mapa.obtenerPosicionableEn(jugador.getPosicion());
-    Posicionable* vacio = mapa.obtenerPosicionableEn(posicionIni);
-    if (vacio == nullptr)
-        lugar_vacio = true;
-    
-    std::cout << resultado->getPosicion().x << std::endl;
-    std::cout << posicionIni.x << std::endl;
-    std::cout << resultado->getPosicion().y << std::endl;
-    std::cout << posicionIni.y << std::endl;
-    std::cout << lugar_vacio << std::endl;
+
 }
 
 ServerHolder::~ServerHolder(){
