65219b6fa4041388de0d201664af9e1eb3d0af75
Andrés Carretero <andi_carretero@hotmail.com>
2020-12-26 10:14:31 -0300

    Add files via upload

---
+++ ThKeyReader.cpp
@@ -0,0 +1,59 @@
+#include "ThKeyReader.h"
+
+#include <Protocol.h>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+ThKeyReader::ThKeyReader(ThSender& th_sender):
+        Thread(), th_sender(th_sender){
+}
+
+void ThKeyReader::run(){
+    SDL_bool done = SDL_FALSE;
+    int id = th_sender.getId();
+    Protocol protocol(id);
+    SDL_Event event;
+    const Uint8 *keys = SDL_GetKeyboardState(NULL);    
+    while (!done) {
+
+
+        if(keys[SDL_SCANCODE_RIGHT]){
+            protocol.moveInDirection(
+                Protocol::direction::ROTATE_RIGHT);
+            th_sender.push(protocol);
+        }
+        if(keys[SDL_SCANCODE_LEFT]){
+            protocol.moveInDirection(
+                Protocol::direction::ROTATE_LEFT);
+            th_sender.push(protocol);
+        }
+        if(keys[SDL_SCANCODE_UP]){
+            protocol.moveInDirection(
+                Protocol::direction::FORWARD);
+            th_sender.push(protocol);
+        }
+        if(keys[SDL_SCANCODE_DOWN]){
+            protocol.moveInDirection(
+                Protocol::direction::BACKWARD);
+            th_sender.push(protocol);
+        }
+
+        while (SDL_PollEvent(&event)) { 
+
+          switch(event.type) {
+            case SDL_QUIT: {
+              done = SDL_TRUE;
+            }
+          }
+        }
+        SDL_Delay(20);
+    }
+}
+
+void ThKeyReader::stop(){
+    is_running = false;
+}
+
+ThKeyReader::~ThKeyReader(){
+}
