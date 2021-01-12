d6ca0d2d75e84b6ad373ebc0bd3737da1d9381c0
Gaston Parente <gaston.parente@globant.com>
2021-01-12 17:39:17 -0300

    Ventanas de lanzar partida y esperar a que se lance

--- client_src/ClientHolder.h
+++ client_src/ClientHolder.h
@@ -19,7 +19,6 @@ private:
     void setId(Protocol& protocol);
     void processReception(Protocol& protocol);
     void createGameModel(int map_id, int id_user_protocol, int game_id);
-    void launchGame();
 public:
     ClientHolder();
     void logginScreen();
@@ -27,6 +26,7 @@ public:
     void crearPartida(const std::string& id_mapa, int& game_id);
     void unirseAPartida(std::string& id_partida);
     void logged(std::string& nombre, std::string& puerto, std::string& servidor);
+    void launchGame();
     ~ClientHolder();
 };
 
