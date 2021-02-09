ef81806068001c8f2ef619d9407f02a531959c0d
Cceecilia <cceciliaroldan@gmail.com>
2021-02-09 03:56:33 -0300

    Agrego descripciones de las funciones

--- server_src/ThGameEvents.h
+++ server_src/ThGameEvents.h
@@ -15,9 +15,17 @@ class ThGameEvents : public Thread{
     
     public:
     explicit ThGameEvents(BlockingQueue<Protocol>& game_model_queue);
+    
+    // Procesa los eventos
     virtual void run() override;
+    
+    // Para el hilo
     virtual void stop() override;
+    
+    // Agrega el evento a la lista de eventos
     void add(Event* event);
+    
+    // Procesa cada evento de la lista de eventos
     void process();
     ~ThGameEvents();
 };
