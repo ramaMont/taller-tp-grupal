e182c9a825892e42a529380e86c2347f35de4742
Rama-PC <ramaa196@gmail.com>
2021-02-11 22:05:44 -0300

    saco coreutils de dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get install coreutils cmake build-essential qt5-default libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev liblua5.3-dev libyaml-cpp-dev libsdl2-mixer-dev
+sudo apt-get install cmake build-essential qt5-default libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev liblua5.3-dev libyaml-cpp-dev libsdl2-mixer-dev
