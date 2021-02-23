b378afed914b312274ee609f83ad5a6fb74cede7
Rama-PC <ramaa196@gmail.com>
2021-02-22 21:51:47 -0300

    cambio copia de carpeta data a /build/data

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ mkdir -m 777 -p ~/.Wolfenstein
 sudo install -m 777 ./build/client_src/Wolfenstein_client_run /usr/bin
 sudo install -m 777 ./build/server_src/Wolfenstein_server_run /usr/bin
 sudo install -m 777 ./build/editor_src/Wolfenstein_editor_run /usr/bin
-cp -r data ~/.Wolfenstein/
+cp -r build/data ~/.Wolfenstein/
 sudo chmod 777 ~/.Wolfenstein/data/maps/*
